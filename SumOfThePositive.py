2a346794a454741a0a86df5c8b33b21c3ceefddd
ednayssell7 <ednayrr@gmail.com>
2021-03-30 23:52:22 -0700

    added comments, sum positive numbers of a list or return 0 if the list is empty or has negative numbers

--- SumOfThePositive.py
+++ SumOfThePositive.py
@@ -2,10 +2,13 @@ def positive_sum(arr):
     # Your code here
     for i in arr:
         if  i>0:
+            #here we sum all the positive numbers
             p_sum= sum(x for x in arr if x > 0)
             return print(p_sum)
         elif i<=0:
+            #returns 0 if its not a positive number
             return print("0")
+    #if list is empty return 0
     if len(arr)==0:
             return print("0")
     
